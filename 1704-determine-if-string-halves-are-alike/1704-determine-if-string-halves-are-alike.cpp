9fa053961ab595bb5389079a88345e02e08d2cff
Ajay122002 <91839316+Ajay122002@users.noreply.github.com>
2022-12-02 11:57:22 +0530

    Time: 11 ms (19.98%), Space: 6.5 MB (83.20%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -6,8 +6,10 @@ public:
         int mid = S.size() / 2, ans = 0;
         for (int i = 0, j = mid; i < mid; i++, j++) {
             if (~vowels.find(S[i])) ans++;
+            //cout<<(~vowels.find(S[i]))<<" "<<(~vowels.find(S[j]))<<endl;
             if (~vowels.find(S[j])) ans--;
         }
+        cout<<(vowels.find(S[2]))<<endl;
         return ans == 0;
     }
 };
