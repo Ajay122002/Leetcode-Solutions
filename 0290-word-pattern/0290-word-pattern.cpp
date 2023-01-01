f7874bddf4ee18be74c23e5dd13ca817bec2ee50
Ajay122002 <91839316+Ajay122002@users.noreply.github.com>
2023-01-01 19:11:05 +0530

    Time: 4 ms (32.33%), Space: 6.4 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    bool wordPattern(string p, string s) 
-    {
-        int n=p.size();
-        int i=0;
-        stringstream ss(s);
-        string token;
+    bool wordPattern(string p, string s) {
+        map<char,string>mp;
+        set<string>st;
         vector<string>vec;
-        while(ss>>token)
-        {
-            vec.push_back(token);
+        stringstream ss(s);
+        string word;
+        while(ss>>word){
+            vec.push_back(word);
         }
         if(vec.size()!=p.size())
-        {
             return false;
-        }
-        unordered_map<char,string>mp;
-        set<string>sets;
-        for(i=0;i<n;i++)
-        {
-            char ch = p[i];
-            // if current character is present in the 
-            // map 
-            if(mp.find(ch)!=mp.end())
-            {
-                // check for the value of that character 
-                if(mp[ch]!=vec[i])
-                {
+        for(int i=0;i<p.size();i++){
+            if(mp[p[i]]==""){
+                if(st.count(vec[i])>0)
                     return false;
-                }
-            }
-            else
-            {
-                 // "abba"
-                //"dog dog dog dog"
-                // for this test case we need to take care of the strings also 
-                // so we use the set , to know whether is the string repeated or                   //not 
-            
-                // if string repeated then return false;
-                if(sets.count(vec[i])>0)
-                {
+                st.insert(vec[i]);
+                mp[p[i]]=vec[i];
+            }else{
+                if(mp[p[i]]!=vec[i])
                     return false;
-                }
-                // else just map the string with the given character and
-                // insert it into the set 
-                mp[ch]=vec[i];
-                sets.insert(vec[i]);
             }
         }
         return true;
