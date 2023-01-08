fbc56b4abc9860d668d9c43454dde2a6ee5602da
Ajay122002 <91839316+Ajay122002@users.noreply.github.com>
2023-01-08 19:04:04 +0530

    Time: 36 ms (88.22%), Space: 12.5 MB (70.67%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-    int maxPoints(vector<vector<int>>& points) {
-        
+    int maxPoints(vector<vector<int>>& points) 
+    {
         int n=points.size();
-        if(n<2)return n;
+        if(n<2) return n;
         int maxCounter=0;
-        
-        for(int i=0;i<n-1;i++){
-            map<pair<int,int>,int>slopeMap;
-            int overlappingPoints=0,verticalPoints=0,counter=0;
-            for(int j=i+1;j<n;j++){
-                if(points[i][0] == points[j][0] && points[i][1] == points[j][1])
-                    overlappingPoints++;
-                else if(points[i][0] == points[j][0])
-                    verticalPoints++;
-                else{
-                    int yDiff=points[j][1] - points[i][1];
-                    int xDiff=points[j][0] - points[i][0];
-                    
-                    int hcf=__gcd(yDiff,xDiff);
-                    yDiff=yDiff/hcf;
-                    xDiff=xDiff/hcf;
-                    
-                    pair<int,int>slope=make_pair(xDiff,yDiff);
-                    slopeMap[slope]++;
-                    counter=max(counter,slopeMap[slope]);
+        // we are picking one point from the vector 
+        for(int i=0;i<n-1;i++)
+        {
+            map<pair<int,int>,int>SlopeMap;
+            int overlapping=0,verticalpoints=0,counter=0;
+            for(int j=i+1;j<n;j++)
+            {
+                if(points[i][0]==points[j][0] && points[i][1]==points[j][1])
+                {
+                    overlapping++;
                 }
-                counter=max(counter,verticalPoints);
+                else if(points[i][0]==points[j][0])
+                {
+                    verticalpoints++;
+                }
+                else
+                {
+                    int xdiff=points[j][0]-points[i][0];
+                    int ydiff=points[j][1]-points[i][1];
+                    int hcf=__gcd(ydiff,xdiff);
+                    xdiff=xdiff/hcf;
+                    
+                    ydiff=ydiff/hcf;
+                    
+                    pair<int,int> slope= make_pair(ydiff,xdiff);
+                    
+                    SlopeMap[ slope ]++;
+                    
+                    counter=max(counter,SlopeMap[ slope ]);
+                }
+                counter=max(counter,verticalpoints);
             }
-            maxCounter=max(maxCounter,counter+overlappingPoints+1);
+            maxCounter=max(maxCounter,counter+overlapping+1);
+
         }
-        return maxCounter;
+        return maxCounter;   
     }
 };
